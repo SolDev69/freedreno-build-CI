567394d69b72f4c45131d4f1509e19c1a0a8dcdc
Maksim Belov <45949002+artdeell@users.noreply.github.com>
2025-11-18 12:05:40 +0300

    Build tagged mesa 25.3.0

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -10,7 +10,7 @@ magiskdir="$workdir/turnip_module"
 ndkver="android-ndk-r28c"
 ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
 sdkver="34"
-mesasrc="https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip"
+mesasrc="https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-25.3.0/mesa-mesa-25.3.0.zip"
 
 clear
 
